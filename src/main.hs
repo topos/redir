916a0c531f4284513697a1b16e18ada01e24e997
ml <slacket@gmail.com>
2014-06-05 00:58:05 -0400

    location header

--- src/main.hs
+++ src/main.hs
@@ -3,47 +3,25 @@
 module Main where
 
 import Network.Wai
+import Network.HTTP.Types (status200)
 import Network.Wai.Handler.Warp (run)
+import Control.Exception (try)
+import Data.Either (isLeft,isRight)
+import Data.Maybe (fromMaybe)
 import Data.ByteString (ByteString)
-import qualified Data.ByteString.Lazy as L
-import Data.Enumerator (consume, Iteratee)
-import Blaze.ByteString.Builder (fromLazyByteString)
-
-main :: IO ()
-main = putStrLn "http://localhost:3000/" >> run "Webkit Sample" app
+import Data.Text (Text)
+import Data.Text.Encoding (encodeUtf8)
+import qualified Data.Map as Map
+import qualified Data.Map.Strict as M
 
 app :: Application
-app req = case pathInfo req of
-    "/post/" -> do
-        bss <- consume
-        postResponse $ L.fromChunks bss
-    _ -> indexResponse
+app req = do
+  let ps = pathInfo req
+      k = if null ps then "" else ps !! 0
+      h = M.fromList [("","http://hello.world.com"), ("hi","http://hi.there.io")]
+      r = case M.lookup k h of
+            Just r' -> r'
+            Nothing -> encodeUtf8 k
+  return $ responseLBS status200 [("Content-Type", "text/plain"), ("Location", r)] ""
 
-indexResponse :: Iteratee ByteString IO Response
-indexResponse = return $ ResponseFile
-    status200
-    [("Content-Type" , "text/html")]
-    "index.html"
-
-postResponse :: L.ByteString -> Iteratee ByteString IO Response
-postResponse lbs = return $ ResponseBuilder
-    status200
-    [("Content-Type", "text/plain")]
-    (fromLazyByteString lbs)
-
--- import Network.Wai
--- import Network.HTTP.Types (status200)
--- import Network.Wai.Handler.Warp (run)
--- import Data.Maybe (fromMaybe)
--- import qualified Data.Map as Map
--- import qualified Data.Map.Strict as M
-
--- app _ = do
---   let h = M.fromList [("/","Hello, World?"),("/hi","Hi, there.")]
---       r = case M.lookup "/hi" h of
---             Just r' -> r'
---             Nothing -> "Lions! Tigers! And bears! Oh, my!"
---   return $ responseLBS status200 [("Content-Type", "text/plain")] r
-
--- main = run 8080 app
-
+main = run 8080 app
