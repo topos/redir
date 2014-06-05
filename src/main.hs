c8d7228bb9d1d7004145a0957dea50382db98a3f
ml <slacket@gmail.com>
2014-06-05 01:28:51 -0400

    trivially working

--- src/main.hs
+++ src/main.hs
@@ -3,7 +3,7 @@
 module Main where
 
 import Network.Wai
-import Network.HTTP.Types (status200)
+import Network.HTTP.Types (status200,status301)
 import Network.Wai.Handler.Warp (run)
 import Control.Exception (try)
 import Data.Either (isLeft,isRight)
@@ -18,10 +18,10 @@ app :: Application
 app req = do
   let ps = pathInfo req
       k = if null ps then "" else ps !! 0
-      h = M.fromList [("","http://hello.world.com"), ("hi","http://hi.there.io")]
+      h = M.fromList [("hs","http://reddit.com/r/haskell"),("sci","http://hi.there.io"),("apple", "http://apple.com")]
       r = case M.lookup k h of
             Just r' -> r'
-            Nothing -> encodeUtf8 k
-  return $ responseLBS status200 [("Content-Type", "text/plain"), ("Location", r)] ""
+            Nothing -> "http://phys.org"
+  return $ responseLBS status301 [("Content-Type", "text/plain"), ("Location", r)] ""
 
 main = run 8080 app
