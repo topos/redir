a08966b771da0f79b73c8a31339396a82666cc91
ml <slacket@gmail.com>
2014-08-26 15:40:40 -0400

    removed extraneous imports

--- src/Main.hs
+++ src/Main.hs
@@ -4,9 +4,8 @@ module Main where
 import Data.ByteString.Char8 (pack)
 import Data.Text (unpack)
 import Network.HTTP.Types (status200,status301,status302)
-import Network.Wai (Application,pathInfo,responseLBS,responseStream)
+import Network.Wai (Application,pathInfo)
 import Network.Wai.Handler.Warp (run)
-import Blaze.ByteString.Builder (fromByteString)
 import Pipes ((>->),yield)
 import Pipes.Wai (Application,Flush(..),producerRequestBody,responseProducer)
 import qualified Pipes.Prelude as P
@@ -28,31 +27,3 @@ app redirects req res = do
                         Just pair -> pair
                         Nothing -> ("-[default]-", 0)
   res $ responseProducer status200 [("Content-Type", "text/plain"),("Location", pack url)] $ yield Flush
-
--- import Data.ByteString.Char8 (pack)
--- import Data.Text (unpack)
--- import Network.Wai (Application,pathInfo,responseLBS,responseStream)
--- import Network.HTTP.Types (status200,status301,status302)
--- import Network.Wai.Handler.Warp (run)
--- import Blaze.ByteString.Builder (fromByteString)
--- import qualified Data.Map as M
--- import qualified Data.Yaml.Etc.Config as C
---
--- main :: IO ()
--- main = run 8080 app
-
--- app :: Application
--- app req res = do
---   y <- C.yaml "/etc/redir.yml"
---   let pi = pathInfo req
---       k = if pi == [] then
---               "url"
---           else
---               unpack $ head pi
---       rs = C.redirs y
---       m = M.fromList $ map (\r->(C.src r,(C.dst r, C.statusCode r))) rs
---       key = "http://" ++ k
---       (url, status) = case M.lookup (key) m of
---                         Just pair -> pair
---                         Nothing -> ("-[default]-", 0)
---   res $ responseLBS status200 [("Content-Type", "text/plain"), ("Location", pack url)] $ ""
